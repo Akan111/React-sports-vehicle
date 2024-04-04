1ca9f62efe84670b082a242a9d1b8f9f01c3d446
Akan380 <akan380@gmail.com>
2024-04-04 20:46:17 +0100

    finished the liked library section

--- react-sports-car-website/src/pages/Library.jsx
+++ react-sports-car-website/src/pages/Library.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./library.css";
+import bgImg from "../images/mclaren-1.jpg";
+import PageTitle from "../components/PageTitle";
+import { AppContext } from "../App";
+import { Link } from "react-router-dom";
+import VehicleCard from "../components/VehicleCard";
 
 function Library() {
-  return <div>Library</div>;
+  const { library: cars } = useContext(AppContext);
+  return (
+    <div id="library" className="page library">
+      <img src={bgImg} alt="" className="img-fluid page-img" />
+      <div className="container">
+        <PageTitle
+          title="Vehicles Library"
+          subtitle="Here are your vehicles collections"
+        />
+        <div className="row">
+          {cars && cars.length === 0 ? (
+            <div className="text-center">
+              <h3 className="empty">You have no collection</h3>
+              <Link className="browse" to="/vehicles">
+                Browse Vehicles
+              </Link>
+            </div>
+          ) : (
+            cars.map((car) => <VehicleCard key={car._id} car={car} />)
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Library;
